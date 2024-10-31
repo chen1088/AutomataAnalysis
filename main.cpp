73538a1da095c37fd9e4e7d30f968fddaea50ded
Chen Xu <chenxu1088@gmail.com>
2024-10-31 08:00:43 -0400

    boost and gmp installed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <boost/dynamic_bitset.hpp>
-
+#include <gmp.h>
 int main(int, char**){
     boost::dynamic_bitset<> db(10);
     db.set(3);
     std::cout << db<<std::endl;
     std::cout << "Hello, from ProductAutomata!\n";
+    mpz_t a;
+    mpz_init(a);
+    mpz_set_ui(a, 10);
+    mpz_out_str(stdout,10,a);
+    std::cout << a << std::endl;
 }
