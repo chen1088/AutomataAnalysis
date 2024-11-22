84c3d356768765f170a1142d119e5b4fba781a12
Chen Xu <chenxu1088@gmail.com>
2024-11-22 16:48:56 -0500

    urgf mul

--- urgf.cpp
+++ urgf.cpp
@@ -25,3 +25,18 @@ urgf urgf::operator+(urgf& other)
     fmpz_poly_div(result.denominator, result.denominator, temp3);
     return result;    
 }
+urgf urgf::operator*(urgf& other)
+{
+    // f1(x) = g1(x)/h1(x)
+    // f2(x) = g2(x)/h2(x)
+    // f3(x) = f1(x) * g2(x) = g1(x)g2(x)/h1(x)h2(x)
+    urgf result;
+    fmpz_poly_t temp1, temp2;
+    fmpz_poly_mul(temp1, this->numerator, other.numerator);
+    fmpz_poly_mul(result.numerator, temp1, other.numerator);
+    fmpz_poly_mul(result.denominator, this->denominator, other.denominator);
+    fmpz_poly_gcd(temp2, result.numerator, result.denominator);
+    fmpz_poly_div(result.numerator, result.numerator, temp2);
+    fmpz_poly_div(result.denominator, result.denominator, temp2);
+    return result;
+}
