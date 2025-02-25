e828e127c8260dde2b5b31f8c395cb6e19011e80
Chen Xu <chenxu1088@gmail.com>
2025-02-25 10:35:55 -0500

    Not working.

--- RegPDFA.h
+++ RegPDFA.h
@@ -1,12 +1,17 @@
 #pragma once
-#include "PDFA.h"
+#include<map>
+#include<vector>
+#include <queue>
+using namespace std;
 #include<boost/dynamic_bitset/dynamic_bitset.hpp>
-class RegPDFA :
-    public PDFA
+using namespace boost;
+class RegPDFA
 {
 public:
-   int maxidx;
-   map<boost::dynamic_bitset<>, boost::dynamic_bitset<>> congruence;
+   bool is_end = false;
+   unsigned int size;
+   map<dynamic_bitset<>, dynamic_bitset<>> congruence0;
+   map<dynamic_bitset<>, dynamic_bitset<>> congruence1;
    // for <k,v> pairs we only record the v's whose k's are more than 1
    // A complete congruence is that for any string s there can be three cases:
    // 1. s is in the map, this is good.
@@ -16,15 +21,15 @@ public:
    RegPDFA();
    RegPDFA(unsigned int s);
    void reset_enumeration();
-   vector<unsigned int> get_transitions_0();
-   vector<unsigned int> get_transitions_1();
    void next();
+   dynamic_bitset<> get_next_state(dynamic_bitset<> currentstate, dynamic_bitset<> str);
    static map<boost::dynamic_bitset<>,boost::dynamic_bitset<>> compute_product(map<boost::dynamic_bitset<>, boost::dynamic_bitset<>> a, map<boost::dynamic_bitset<>, boost::dynamic_bitset<>> b)
    {
       // we might need to use bi-map from boost library
       // we should assume that the transition table is complete
       map<boost::dynamic_bitset<>, boost::dynamic_bitset<>> res;
       map<pair<boost::dynamic_bitset<>,boost::dynamic_bitset<>>, boost::dynamic_bitset<>> temp;
+      queue<pair<boost::dynamic_bitset<>, boost::dynamic_bitset<>>> q;
       for (auto i : a)
       {
          for (auto j : b)
