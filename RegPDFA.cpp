c4b155bd36b82f6cdd40c10cb679a41285fd8662
Chen Xu <chenxu1088@gmail.com>
2025-03-10 17:56:24 -0400

    Does it work?

--- RegPDFA.cpp
+++ RegPDFA.cpp
@@ -35,7 +35,28 @@ dynamic_bitset<> RegPDFA::reduce(dynamic_bitset<> currentstate, dynamic_bitset<>
       return congruence1[currentstate];
    }
    // 2. s is not in the map, and s can be reduced, we solve the congruence of s.
-
+   auto strcopy = str;
+   while(strcopy.count() > 0)
+   {
+      auto temp = strcopy;
+      strcopy = dynamic_bitset<>(size);
+      for(auto i = 0; i < size; i++)
+      {
+         if(temp[i] == 1)
+         {
+            if(congruence0.find(currentstate) != congruence0.end())
+            {
+               strcopy |= congruence0[currentstate];
+            }
+            if(congruence1.find(currentstate) != congruence1.end())
+            {
+               strcopy |= congruence1[currentstate];
+            }
+         }
+      }
+      if(strcopy == temp)
+         break;
+   }
    // 3. s is not in the map, and s cannot be reduced, by default the sc(c is any character) is congruent to sc.
    return res;
 }
@@ -49,15 +70,36 @@ bool RegPDFA::check_completeness()
    map<int,set<dynamic_bitset<>>> q;
    for (auto i : congruence0)
    {
-      q[i.first.count()].insert(i.first);
+      auto t = i.first;
+      t.push_back(0);
+      q[t.count()].insert(t);
    }
    for (auto i : congruence1)
    {
-      q[i.first.count()].insert(i.first);
+      auto t = i.first;
+      t.push_back(1);
+      q[t.count()].insert(t);
    }   
    auto ks = std::views::keys(q);
    auto vectorkey = vector<int>(ks.begin(),ks.end());
    // from the largest to the smallest
-   
+   int max = *max_element(vectorkey.begin(),vectorkey.end());
+   for(auto i = max; i > 0; i--)
+   {
+      for(auto j : q[i])
+      {
+         auto jj = j.flip(j.count()-1); // check this flip.
+         if(q[i].find(jj) == q[i].end())
+            return false; // if j is not complete, return false
+         else
+         {
+            auto jjj = j; // check this copy.
+            jjj.pop_back();
+            q[i-1].insert(jjj);
+            q[i].erase(jj);
+            q[i].erase(j);
+         }
+      }
+   }
    return true;
 }
