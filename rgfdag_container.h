04ecf570bf6a807ceaa575c0b30d3639925671ba
Chen Xu <chenxu1088@gmail.com>
2025-09-21 09:04:31 -0400

    todo: compress and merge

--- rgfdag_container.h
+++ rgfdag_container.h
@@ -18,10 +18,23 @@ public:
       
    }
 
+   void compress()
+   {
+      auto comparator = [](const rgfdag<T>* a, const rgfdag<T>* b) {
+         // solve c = a - b
+         auto c = a->resolvetorgf_nocache() - b->resolvetorgf_nocache();
+         // compute the first non-zero coefficient of c
+         auto coeffs = c->rgf_instance;
+         return fmpz_cmp(c.get_first_coefficient_in_fmpz(), b.get_first_coefficient_in_fmpz()) < 0;
+      };
+      map<rgfdag<T>*, rgfdag<T>*, decltype(comparator)> unique_rgfdags(comparator);
+
+   }
 private:
    
    RegPDFA* pdfa;
    map<dynamic_bitset<>, map<dynamic_bitset<>, rgfdag<T> *>> pairwise_rgfdags;
+   
    void merge_rdfdag(rgfdag<T>* dag)
    {
       for(auto i: pairwise_rgfdags)
